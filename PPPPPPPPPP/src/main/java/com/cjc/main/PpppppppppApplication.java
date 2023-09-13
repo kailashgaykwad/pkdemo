24a57cc9674e3c576be66e5550e973ac456f4c68
kailashgaykwad <kailashgayakwad2999@gmail.com>
2023-09-13 06:02:45 -0700

    ppkdemo

---
+++ PPPPPPPPPP/src/main/java/com/cjc/main/PpppppppppApplication.java
@@ -0,0 +1,13 @@
+package com.cjc.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpppppppppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpppppppppApplication.class, args);
+	}
+
+}
